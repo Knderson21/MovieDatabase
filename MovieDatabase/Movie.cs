62c68521bbbe2656bbf8d2bac72b3460c66e370f
Krista <knderson21@gmail.com>
2022-07-07 08:43:40 -0400

    MovieDatabase

---
+++ MovieDatabase/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase
+{
+    internal class Movie
+    {
+        //properties
+        public string Title { get; set; }
+        public string Category { get; set; }
+
+        public Movie(string _title, string _category)
+        {
+            Title = _title;
+            Category = _category;
+        }
+
+
+        //methods
+        public virtual string DisplayMovies(string category)
+        {
+            return Console.WriteLine($" {Title}  ");
+        }
+    }
+}
---
+++ MovieDatabase/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+using MovieDatabase;
+
+Console.WriteLine("Welcome to the Movie List Application!");
+Console.WriteLine("There are 10 movies you can pick from.");
+
+List<Movie> allTheMovies = new List<Movie>();
+allTheMovies.Add(new Movie("Iron Man", "Action"));
+allTheMovies.Add(new Movie("Fast and Furious", "Action"));
+allTheMovies.Add(new Movie("Starship Troopers", "SciFi"));
+allTheMovies.Add(new Movie("Titanic", "Romance"));
+allTheMovies.Add(new Movie("Jaws", "Thriller"));
+allTheMovies.Add(new Movie("Inception", "Thriller"));
+allTheMovies.Add(new Movie("Ready Player One", "SciFi"));
+allTheMovies.Add(new Movie("The Notebook", "Romance"));
+allTheMovies.Add(new Movie("Rent", "Musicals"));
+allTheMovies.Add(new Movie("Hairspray", "Musicals"));
+
+
+Console.WriteLine("What category are you interested in? \nYou can pick from Action, SciFi, Thriller, Romance, or Musicals");
+
+while (true)
+{
+    string category = Console.ReadLine();
+    if (category == "Action" || category == "SciFi" || category == "Romance" || category == "Thriller" || category == "Musicals")
+    {
+        foreach (Movie movie in allTheMovies)
+        {
+            Console.WriteLine(movie.DisplayMovies());
+        }
+        break;
+    }
+    else
+    {
+        Console.WriteLine("That is not a valid category. Please select a category:");
+    }
+}
+
+
